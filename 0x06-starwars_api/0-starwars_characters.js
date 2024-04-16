c43397b8e4998e9f24330d73f0899464638578c9
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-04-16 15:40:05 +0100

    Add Star Wars API implementation and update dependencies

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,22 @@
-#!/usr/bin/node
-const request = require('request');
+#!/usr/bin/env node
+const util = require('node:util');
+const request = util.promisify(require('request'));
 const argv = process.argv;
 const FILM_ID = argv[2];
-async function printStarWarsCharacters(filmId) {
+async function printStarWarsCharacters (filmId) {
   const BASE_URL = 'https://swapi-api.alx-tools.com/api/films/';
-  request(BASE_URL + FILM_ID, async function (error, response, body) {
-    if (error) {
-      console.log(error);
-    }
-    body = JSON.parse(body);
-    const characters = body.characters;
+  try {
+    let response = await (await request(BASE_URL + filmId)).body;
+    response = JSON.parse(response);
+    const characters = response.characters;
+
     for (const i of characters) {
-      request(i, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        }
-        body = JSON.parse(body);
-        console.log(body.name);
-      });
+      const characterUrl = i;
+      let character = await (await request(characterUrl)).body;
+      character = JSON.parse(character);
+      console.log(character.name);
     }
-  });
+  } catch (error) {}
 }
 
 printStarWarsCharacters(FILM_ID);
